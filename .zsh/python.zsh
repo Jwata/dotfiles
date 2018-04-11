826b3cfaa72ded80132016c223e035230b3cc217
Jwata <watanabe0621@gmail.com>
2018-04-11 17:22:33 +0900

    update

--- .zsh/python.zsh
+++ .zsh/python.zsh
@@ -1,13 +1,16 @@
+# export PYTHONSTARTUP=~/.pythonrc.py
+# export PATH="$HOME"/miniconda3/bin:$PATH
+# 
+# if [ `command -v conda` ];then
+# elif [ `command -v pyenv` ];then
+#   export PYENV_ROOT=/usr/local/var/pyenv
+#   export PATH="$PYENV_ROOT/bin:$PATH"
+#   pyenv() {
+#     eval "$( command pyenv init - )"
+#     eval "$(pyenv virtualenv-init -)"
+#     pyenv "$@"
+#   }
+# fi
 export PYTHONSTARTUP=~/.pythonrc.py
-export PATH="$HOME"/miniconda3/bin:$PATH
-
-if [ `command -v conda` ];then
-elif [ `command -v pyenv` ];then
-  export PYENV_ROOT=/usr/local/var/pyenv
-  export PATH="$PYENV_ROOT/bin:$PATH"
-  pyenv() {
-    eval "$( command pyenv init - )"
-    eval "$(pyenv virtualenv-init -)"
-    pyenv "$@"
-  }
-fi
+. /usr/local/miniconda3/etc/profile.d/conda.sh
+export PATH=/usr/local/miniconda3/bin:"$PATH"
--- .zshrc
+++ .zshrc
@@ -12,9 +12,4 @@ done
 
 source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-#export NVM_DIR="$HOME/.nvm"
-#. "/usr/local/opt/nvm/nvm.sh"
-export NVM_LAZY_LOAD=true
-antigen bundle lukechilds/zsh-nvm
-
 export PATH="/usr/local/opt/qt/bin:$PATH"
